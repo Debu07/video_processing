bdf1e5ef669466801c48f7edee6a055855850144
StrongestAvenger <38360953+Debu07@users.noreply.github.com>
2019-06-25 11:11:10 +0100

    Update video.py

--- video.py
+++ video.py
@@ -1,15 +1,15 @@
-import cv2
-video=cv2.VideoCapture(0)
-count=0
-while True:
+import cv2 #import openCV
+video=cv2.VideoCapture(0) #here 0 is for web camera if you are using other camera you can use 1,2 etc as per device
+count=0 # to count the number of frames generated
+while True: #loop for video
     count+=1
-    check , frame=video.read()
-    print(check)
-    print(frame)
-    cv2.imshow("capturing",frame)
-    key=cv2.waitKey(1)
-    if key==ord('q'):
-        break
-print(count)
-video.release()
-cv2.destroyAllWindows()
+    check , frame=video.read() # check and frame are boolean and numpy array data type
+    print(check) #it will return true or false
+    print(frame) # it will return n-d numpy array 
+    cv2.imshow("capturing",frame) #window generated to show video procesing
+    key=cv2.waitKey(1) # video generation after every 1 millisecond
+    if key==ord('q'): # to exit the loop by pressing q->quit
+        break 
+print(count) #to print number of frames generated
+video.release() #to release the video Stop the web cam
+cv2.destroyAllWindows() #to exit the window generated in previous steps
